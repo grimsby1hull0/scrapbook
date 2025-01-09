304fadd25a089ad7842ff469f643ff1213f26f0f
Arron Ware <70115287+grimsby1hull0@users.noreply.github.com>
2025-01-09 23:51:48 +0000

    first website and many other bits

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ toggleThemeButton.addEventListener('click', toggleTheme);
 applyTimeBasedTheme();
 
 // Optional: Reapply the time-based theme every minute to stay in sync
-setInterval(applyTimeBasedTheme, 60000); // Check every minute
+setInterval(applyTimeBasedTheme, 60000); // Check every minute aka 60000
 
 
 
@@ -33,28 +33,28 @@ let outputElement = document.getElementById("OTMoutput");
 function OTMpuller() {
   const month = new Date().getMonth() + 1; // (Jan = 0) if not
   if (month === 1) {
-    outputElement.innerHTML = "<div w3-include-html='OTM/1.html'></div>";
+    outputElement.innerHTML = "<div w3-include-html='OTM/1jan.html'></div>";
   } else if (month === 2) {
     outputElement.innerHTML = "<div w3-include-html='OTM/2.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 3) {
     outputElement.innerHTML = "<div w3-include-html='OTM/3.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 4) {
     outputElement.innerHTML = "<div w3-include-html='OTM/4.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 5) {
     outputElement.innerHTML = "<div w3-include-html='OTM/5.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 6) {
     outputElement.innerHTML = "<div w3-include-html='OTM/6.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 7) {
     outputElement.innerHTML = "<div w3-include-html='OTM/7.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 8) {
     outputElement.innerHTML = "<div w3-include-html='OTM/8.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 9) {
     outputElement.innerHTML = "<div w3-include-html='OTM/9.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 10) {
     outputElement.innerHTML = "<div w3-include-html='OTM/10.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 11) {
     outputElement.innerHTML = "<div w3-include-html='OTM/11.html'></div>";
-  } else if (month === 2) {
+  } else if (month === 12) {
     outputElement.innerHTML = "<div w3-include-html='OTM/12.html'></div>";
   } else {
     outputElement.innerHTML = "<p>you have more than 12 months wowza!</p>";
