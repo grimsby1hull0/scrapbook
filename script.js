b3d07c322d0e9a4f965096235ef388dcf82c15fc
Arron Ware <70115287+grimsby1hull0@users.noreply.github.com>
2025-01-09 03:10:29 +0000

    otm

--- script.js
+++ script.js
@@ -24,3 +24,45 @@ applyTimeBasedTheme();
 setInterval(applyTimeBasedTheme, 60000); // Check every minute
 
 
+
+
+
+// Function find events from previous month and display them (on this day in my own personal history)
+let outputElement = document.getElementById("OTMoutput");
+
+function OTMpuller() {
+  const month = new Date().getMonth() + 1; // (Jan = 0) if not
+  if (month === 1) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/1.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/2.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/3.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/4.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/5.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/6.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/7.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/8.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/9.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/10.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/11.html'></div>";
+  } else if (month === 2) {
+    outputElement.innerHTML = "<div w3-include-html='OTM/12.html'></div>";
+  } else {
+    outputElement.innerHTML = "<p>you have more than 12 months wowza!</p>";
+  }
+  
+
+  
+}
+
+// Call the function to update the content
+OTMpuller();
